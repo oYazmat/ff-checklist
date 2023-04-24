4a14bd9b72cae4795446a5dd43dca21c4661cb5b
Yazmat <j.yazmat@gmail.com>
2023-04-24 13:23:58 -0400

    added badges note

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,7 +1,18 @@
-import { Image } from "@chakra-ui/react";
+import { Image, Stack, StackItem, Text } from "@chakra-ui/react";
 
 const Badges = () => {
-  return <Image src="badges/nothing.png" alt="nothing" title="Nothing" />;
+  return (
+    <Stack gap={3}>
+      <StackItem>
+        <Text as="i">
+          Note: Badges are in Beta, they might not work as expected!
+        </Text>
+      </StackItem>
+      <StackItem>
+        <Image src="badges/nothing.png" alt="nothing" title="Nothing" />
+      </StackItem>
+    </Stack>
+  );
 };
 
 export default Badges;
